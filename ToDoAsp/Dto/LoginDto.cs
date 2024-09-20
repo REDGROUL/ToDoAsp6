504e3494b62d7798a10a8568eae23abdcf86e2c5
RED <redgroulman@gmail.com>
2024-09-20 10:41:02 +0300

    dto: reg and auth

---
+++ ToDoAsp/Dto/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoAsp.Dto
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ToDoAsp/Dto/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoAsp.Dto
+{
+    public class RegisterDto
+    {
+
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
