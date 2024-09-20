a6082b6ea8c719c88c39e8d6fc72b132253daa7c
RED <redgroulman@gmail.com>
2024-09-20 10:36:44 +0300

    Добавьте файлы проекта.

---
+++ ToDoAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
