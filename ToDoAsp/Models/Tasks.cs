300bb4e5674cedef1b2608bf1ec9b21b3b6b5199
RED <redgroulman@gmail.com>
2024-09-26 12:01:27 +0300

    models

---
+++ ToDoAsp/Models/Tasks.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoAsp.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string? OwnerId { get; set; }
+        public virtual IdentityUser? Owner { get; set; }
+
+
+        
+    }
+
+
+   
+}
+
