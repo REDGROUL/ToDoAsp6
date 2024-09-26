51ab24a2d08fa063ccc95590e2f30dffc6236c31
RED <redgroulman@gmail.com>
2024-09-26 12:01:36 +0300

    frontend

--- ToDoAsp/Program.cs
+++ ToDoAsp/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ToDoAsp.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+DIConfig.Services(builder);
+
+
+
+
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -14,10 +31,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.Map("/data", [Authorize] () => new { message = "Hello World!" });
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=StaticPages}/{action=Auth}");
 
 app.Run();
--- ToDoAsp/wwwroot/js/site.js
+++ ToDoAsp/wwwroot/js/site.js
@@ -2,3 +2,113 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.onload = () => {
+
+    let loginBtn = document.getElementById("auth");
+    let regBtn = document.getElementById("tryReg");
+
+    let info = document.getElementById("info");
+
+    if (loginBtn) {
+
+
+
+        loginBtn.addEventListener("click", () => {
+            let login = document.getElementById("loginAuth").value;
+            let password = document.getElementById("passwordAuth").value;
+
+            
+            if (login != "" && password != "") {
+
+                let resp = JSON.stringify({
+                    "email": login,
+                    "password": password
+                });
+
+                info.innerText = "Проверяем..."
+
+                fetch("/auth/login", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json' 
+                    },
+                    body: resp,
+
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            window.location.href = '/StaticPages/Tasks'
+                        } else {
+                            info.innerText = "Проверьте логин и пароль!"
+                        }
+
+                        
+                        console.log(response);
+
+                    })
+
+                    .then(data => {
+                        
+
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        info.innerText = "Проверьте лоин и пароль!"
+                      
+                    })
+            }
+            console.log("click")
+
+        })
+
+    }
+
+
+    if (regBtn) {
+
+        regBtn.addEventListener("click", () => {
+            console.log("reg btn")
+            let login = document.getElementById("usernameRed").value;
+            let email = document.getElementById("emailReg").value;
+            let password = document.getElementById("passwordReg").value;
+
+
+            if (login != "" && password != "" && password != "") {
+                info.innerText = "Регистрируем";
+                let resp = JSON.stringify({
+                    "username": login,
+                    "email": email,
+                    "password": password
+                });
+
+                fetch("/auth/register", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: resp,
+
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            info.innerText = "Авторизируйтесь"
+                        } else {
+                            info.innerText = "Логин или почта возможно уже существуют, либо используйте более сложный пароль"
+                        }
+
+                    })
+
+                    .catch(error => {
+                        console.log(error);
+
+                    })
+            }
+            console.log("click")
+
+        })
+
+    }
+
+
+
+};
